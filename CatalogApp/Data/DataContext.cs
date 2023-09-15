ad2d1e99fc3d0078b992b78ce218c3d3ff7064b1
Vladyslav Trach <vladyslav.trach.ki.2020@lpnu.ua>
2023-09-15 16:50:27 +0300

    Connected Database
    +EF
    +CatalogModel
    +DataContext
    +Catalog Controler

---
+++ CatalogApp/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using CatalogApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CatalogApp.Data
+{
+	public class DataContext : DbContext
+	{
+		public DataContext(DbContextOptions<DataContext> options) : base(options)
+		{
+
+		}
+
+		public DbSet<CatalogModel> Catalogs { get; set; }
+	}
+}
---
+++ CatalogApp/Models/CatalogModel.cs
@@ -0,0 +1,9 @@
+﻿namespace CatalogApp.Models
+{
+	public class CatalogModel
+	{
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ParentalDirectoryName { get; set; }
+    }
+}
