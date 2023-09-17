4a34ec76b94fe16a8bdba077c3b7357acf36bcfa
Vladyslav Trach <vladyslav.trach.ki.2020@lpnu.ua>
2023-09-17 14:00:27 +0300

    Working App
    ~Controller
    +EmptyFolder View
    + ViewFolder

--- CatalogApp/Controllers/CatalogController.cs
+++ CatalogApp/Controllers/CatalogController.cs
@@ -1,12 +1,78 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CatalogApp.Data;
+using CatalogApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace CatalogApp.Controllers
 {
-	public class CatalogController : Controller
-	{
-		public IActionResult Index()
-		{
-			return View();
-		}
-	}
+    public class CatalogController : Controller
+    {
+        private readonly DataContext _context;
+
+        public CatalogController(DataContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            // Retrieve the main catalog
+            var mainCatalog = _context.Catalogs.FirstOrDefault(c => c.ParentalDirectoryName == "None");
+
+            if (mainCatalog == null)
+            {
+                // Handle the case where the main catalog is not found.
+                return NotFound();
+            }
+
+            // Retrieve sub-catalogs for the main catalog   
+            var subCatalogs = _context.Catalogs.Where(c => c.ParentalDirectoryName == mainCatalog.Name).ToList();
+
+            // Create a list to hold the main catalog and its sub-catalogs
+            List<CatalogModel> mainCatalogWithSubCatalogs = new List<CatalogModel>();
+
+            // Add the main catalog to the list
+            mainCatalogWithSubCatalogs.Add(mainCatalog);
+
+            // Add the sub-catalogs to the list
+            mainCatalogWithSubCatalogs.AddRange(subCatalogs);
+
+            return View(mainCatalogWithSubCatalogs);
+        }
+
+        public IActionResult ViewFolder(string name)
+        {
+            // Retrieve the main catalog
+            var mainCatalog = _context.Catalogs.FirstOrDefault(c => c.Name == name);
+
+            if (mainCatalog == null)
+            {
+                // Handle the case where the main catalog is not found.
+                return NotFound();
+            }
+
+            // Retrieve sub-catalogs for the main catalog
+            var subCatalogs = _context.Catalogs.Where(c => c.ParentalDirectoryName == name).ToList();
+
+            // Create a list to hold the main catalog and its sub-catalogs
+            List<CatalogModel> mainCatalogWithSubCatalogs = new List<CatalogModel>();
+
+            // Add the main catalog to the list
+            mainCatalogWithSubCatalogs.Add(mainCatalog);
+
+            // Add the sub-catalogs to the list
+            mainCatalogWithSubCatalogs.AddRange(subCatalogs);
+
+            if(mainCatalogWithSubCatalogs.Count < 2)
+                return View("EmptyFolder");
+
+            return View(mainCatalogWithSubCatalogs);
+        }
+
+        public IActionResult EmptyFolder()
+        {
+            return View();
+        }
+
+    }
 }
--- CatalogApp/Controllers/HomeController.cs
+++ CatalogApp/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using CatalogApp.Models;
+﻿using CatalogApp.Data;
+using CatalogApp.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -7,20 +8,18 @@ namespace CatalogApp.Controllers
 	public class HomeController : Controller
 	{
 		private readonly ILogger<HomeController> _logger;
+		private readonly DataContext _context;
 
-		public HomeController(ILogger<HomeController> logger)
+		public HomeController(ILogger<HomeController> logger, DataContext context)
 		{
 			_logger = logger;
+			_context = context;
 		}
 
 		public IActionResult Index()
 		{
-			return View();
-		}
-
-		public IActionResult Privacy()
-		{
-			return View();
+			var mainCatalog = _context.Catalogs.Where(c => c.ParentalDirectoryName == "None").FirstOrDefault();
+			return View(mainCatalog);
 		}
 
 		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
